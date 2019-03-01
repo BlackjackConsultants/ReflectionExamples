83549b9b7de9ebc1c168fa1a90fc7ac55edecf1c
jorge perez <jorgeperez00@hotmail.com>
2019-03-01 15:40:26 -0500

    added a test that shows how a collision happens when you have 100000 items

--- ReflectionExamples/ObjectExtensionTest.cs
+++ ReflectionExamples/ObjectExtensionTest.cs
@@ -35,6 +35,26 @@ namespace ReflectionExamples2 {
         }
 
         [TestMethod]
+        public void CheckCollision() {
+            for (int ii = 0; ii < 100; ii++) {
+                Dictionary<int, Individual> dictionary = new Dictionary<int, Individual>();
+                for (int i = 0; i < 100000; i++) {
+                    var individual = CreateLazyIndividual(i);
+                    var hashKey = individual.GetHashCode();
+                    if (!dictionary.ContainsKey(hashKey)) {
+                        // hash not found in dictionary
+                        dictionary.Add(hashKey, individual);
+                    } else {
+                        // hash not found in dictionary
+                        System.Diagnostics.Debug.WriteLine("The collision occurred at {0}", new[] { i.ToString() });
+                        ////Assert.Fail(string.Format("Collision found where i = {0}", i.ToString()));
+                    }
+                }
+            }
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
         public void GenerateObjectPath() {
             var individual = CreateIndividual();
             List<KeyValuePair<string, string>> keys = new List<KeyValuePair<string, string>>();
@@ -45,16 +65,27 @@ namespace ReflectionExamples2 {
             keys.Add(new KeyValuePair<string, string>("state", "Id"));
             keys.Add(new KeyValuePair<string, string>("country", "Id"));
             var path = individual.Phone.GetObjectPath(keys, "Entity");
-			Assert.AreEqual("/Individual[Id=1]/Phone[Id=1&Number=305.333.3333]", path.ToString());
+            Assert.AreEqual("/Individual[Id=1]/Phone[Id=1&Number=305.333.3333]", path.ToString());
         }
 
         private Phone CreatePhone(Individual parent) {
-            var phone = new Phone             {
+            var phone = new Phone
+            {
                 Entity = parent,
                 Id = 1,
                 Number = "305.333.3333"
             };
             return phone;
+        }
+
+        private Individual CreateLazyIndividual(int id) {
+            var individual = new Individual();
+            individual.Id = 1;
+            individual.FirstName = "jorge";
+            individual.LastName = "perez";
+            individual.FileAs = "jp";
+            individual.Id = id;
+            return individual;
         }
 
         private Individual CreateIndividual() {
@@ -71,7 +102,8 @@ namespace ReflectionExamples2 {
         }
 
         private Phone CreatePhone() {
-            return new Phone {
+            return new Phone
+            {
                 Id = 1,
                 Number = "305.555.3333"
             };
@@ -85,7 +117,7 @@ namespace ReflectionExamples2 {
             email1.EmailAddress = "jogito.suarez@yahoo.com";
             emails.Add(email1);
             return emails;
-            
+
         }
 
         private Domain CreateDomain(Email email) {
